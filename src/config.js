569c565706d37334a955ff3357e79c1f5ec5b052
kumar-verily <kumar@verily.com>
2025-07-30 14:07:21 -0700

    Update config.js
    Switched to public backend endpoint

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //Use this file for constants and configuration settings
-// export const API_BASE_URL = "https://synth-production.up.railway.app"
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://synth-production.up.railway.app"
+// export const API_BASE_URL = "http://localhost:8000";
